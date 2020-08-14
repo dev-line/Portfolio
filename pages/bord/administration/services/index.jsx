1ac3ee4c55c5f28c41850839900f5a246b853107
unknown <safsystm@gmail.com>
2020-08-14 13:56:28 +0100

    LastEdit

--- pages/bord/administration/services/index.jsx
+++ pages/bord/administration/services/index.jsx
@@ -90,7 +90,7 @@ export default function Services() {
                   }):<FindingError/>
                 )}
             </div>
-            <div className="position-fixed bottom-0 right-0 w-15 text-center">
+            <div className="position-fixed bottom-0 right-0 w-100 w-md-15 text-center">
               <button
                 type="button"
                 className="btn btn-light w-100 spacer-10 shadow-sm bg-white py-3"
